9b18af7f7cc2315657501bfe2ec36ea82b7706a0
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-06 17:28:07 +0800

    Modify application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
-
-  def after_sign_in_path_for(resource)
-    users_path
-  end
 end
