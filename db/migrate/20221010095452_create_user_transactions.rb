a3d2f062966e99dc70795660bec7196b9dc1641d
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-10 18:35:04 +0800

    feat: Set up the tables

--- db/migrate/20221010095452_create_user_transactions.rb
+++ db/migrate/20221010095452_create_user_transactions.rb
@@ -1,6 +1,12 @@
 class CreateUserTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :user_transactions do |t|
+      t.string :symbol
+      t.string :company_name
+      t.float :shares
+      t.float :price
+      t.float :value
+      t.integer :transaction_type_id
 
       t.timestamps
     end
