e2bdb73f7d4d47486ab79d1e00b65aa0cfe8c651
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-10 16:26:30 +0800

    feat: Associate stock model with the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :stocks
+  
   after_initialize :set_default_role, if: :new_record?
 
   devise :database_authenticatable, :registerable, :validatable, :confirmable
