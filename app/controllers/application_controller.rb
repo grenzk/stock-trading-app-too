75218314e78984d0425f6400a7c6ea3d9c5a49c2
Ellaine Dela Cruz <maellaineanis@gmail.com>
2022-10-11 08:30:20 +0800

    created side bar for stocks list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :set_stocks_data
 
   def set_stocks_data 
-    sectors = ["Retail Trade", "Communications", "Health Services", "Technology Services", "Finance"]
     client = IEX::Api::Client.new
-    @quote = client.quote('MSFT')
-    @list = client.get('/stock/market/collection/sector?collectionName=Wholesale Trade', token: 'sk_00c8f94b232642c3a90df6c727e6b6c5')
-
-    
   end
 
   def after_sign_in_path_for(resource)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+  def most_active
+    client = IEX::Api::Client.new
+    client.stock_market_list(:mostactive)
+  end
+
+  def collection
+    client = IEX::Api::Client.new
+  end
+
+  def symbol_list
+    client = IEX::Api::Client.new
+    client.ref_data_symbols()
+  end
+
+  def company_info(symbol)
+    client = IEX::Api::Client.new
+    company = client.company(symbol)
+    company.company_name
+  end
+
+  def company_logo(symbol)
+    client = IEX::Api::Client.new
+    logo = client.logo(symbol)
+    logo.url
+  end
+
 end
