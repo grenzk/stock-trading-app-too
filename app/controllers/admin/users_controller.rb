d0478f8f36793be5308bb92db7fbd86d9c3f5bec
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-13 15:20:12 +0800

    chore: Require approved

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -20,6 +20,6 @@ class Admin::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :approved)
   end
 end
