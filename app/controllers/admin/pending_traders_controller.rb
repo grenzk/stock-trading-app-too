efc6d1814d0b5c8b7a1fc5cd4f5588710daabb00
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-14 14:46:08 +0800

    feature: Approve pending traders

--- app/controllers/admin/pending_traders_controller.rb
+++ app/controllers/admin/pending_traders_controller.rb
@@ -2,4 +2,13 @@ class Admin::PendingTradersController < ApplicationController
   def index
     @users = User.where(approved: false, role: :trader)
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(approved: :true)
+      redirect_to admin_users_path, notice: 'User has been approved.'
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
 end
