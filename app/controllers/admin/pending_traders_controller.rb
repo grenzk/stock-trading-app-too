31a50535258ca443aae320eb753587572f8a6d2d
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-14 13:25:34 +0800

    feature: Add pending-traders controller

---
+++ app/controllers/admin/pending_traders_controller.rb
@@ -0,0 +1,2 @@
+class Admin::PendingTradersController < ApplicationController
+end
---
+++ test/controllers/admin/pending_traders_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Admin::PendingTradersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
