7d285aa72a08b29f7e895df7371da519bb22c3a4
grenzk <74033991+grenzk@users.noreply.github.com>
2022-10-16 17:42:37 +0800

    chore: Create helper methods

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,19 @@
 class Stock < ApplicationRecord
   belongs_to :user
+
+  validates :symbol, :company_name, presence: true
+
+  def stocks_data(symbol)
+    client =
+      IEX::Api::Client.new(
+        publishable_token:
+          Rails.application.credentials[:iex][:publishable_token],
+        secret_token: Rails.application.credentials[:iex][:secret_token],
+        endpoint: 'https://cloud.iexapis.com/v1'
+      )
+  end
+
+  def check_stocks(symbol)
+    where(symbol: symbol).first
+  end
 end
